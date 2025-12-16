97a0f5b8b23543d59d5d8e335d370b9dc3aa32b4
vedhapprakashni <vedhamuthukumar@gmail.com>
2025-12-16 19:01:45 +0530

    first commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module golang1
+
+go 1.25.5
---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	// This is a placeholder for the main function.
+	fmt.Println("welcome to the booking application")
+	fmt.Println("Get your tickets to attend")
+
+	var confname = "Go conference"
+	fmt.Println("conference name is:", confname)
+
+}
