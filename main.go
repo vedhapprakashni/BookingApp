4b61ae42a1832db815ad4d49ea26904d8dbca819
vedhapprakashni <vedhamuthukumar@gmail.com>
2025-12-16 21:27:35 +0530

    done with basic info

--- main.go
+++ main.go
@@ -1,13 +1,38 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
+	var confname = "Go conference"
+	const conftickets = 50
+	var remainingtickets = 50
+
+	fmt.Printf("conferencetickets is %T,remaining tickets is %T,confname is %T\n", conftickets, remainingtickets, confname)
 	// This is a placeholder for the main function.
-	fmt.Println("welcome to the booking application")
+
+	fmt.Println("welcome to the", confname, "booking application")
+	fmt.Println("We have a total of", conftickets, "tickets and", remainingtickets, "are still available.")
 	fmt.Println("Get your tickets to attend")
 
-	var confname = "Go conference"
-	fmt.Println("conference name is:", confname)
+	var firstname string
+	var lastname string
+	var email string
+	var usertickets int
+
+	fmt.Println("Enter your first name:")
+	fmt.Scan(&firstname)
+
+	fmt.Println("Enter your last name:")
+	fmt.Scan(&lastname)
+
+	fmt.Println("Enter your email:")
+	fmt.Scan(&email)
+
+	fmt.Println("Enter your userticket:")
+	fmt.Scan(&usertickets)
+
+	fmt.Printf("Thank you %v %v for booking %v tickets. You will receive a confirmation email at %v\n", firstname, lastname, usertickets, email)
 
 }
